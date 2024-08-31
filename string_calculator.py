6f15f8de7ab9de020fab5ae94a42602acdeda97d
Ankit Kumar <talk2ankit007@gmail.com>
2024-08-31 12:45:30 +0530

    modified add method to support custom delimiters

--- string_calculator.py
+++ string_calculator.py
@@ -1,7 +1,17 @@
 # string_calculator.py
+import re
+
 def add(numbers: str) -> int:
     if not numbers:
         return 0
-    numbers = numbers.replace('\n', ',')
-    num_list = map(int, numbers.split(','))
+    
+    delimiter = ','
+    
+    if numbers.startswith("//"):
+        parts = numbers.split('\n', 1)
+        delimiter = re.escape(parts[0][2:])
+        numbers = parts[1]
+    
+    numbers = numbers.replace('\n', delimiter)
+    num_list = map(int, numbers.split(delimiter))
     return sum(num_list)
