8a3e04bfc5b36b95705cbdbc39e18aa705006183
Ankit Kumar <talk2ankit007@gmail.com>
2024-08-31 12:43:08 +0530

    added test case to handle newlines between numbers

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -12,6 +12,9 @@ class TestStringCalculator(unittest.TestCase):
     
     def test_two_numbers(self):
         self.assertEqual(add("1,2"), 3)
+    
+    def test_newlines_between_numbers(self):
+        self.assertEqual(add("1\n2,3"), 6)
 
 if __name__ == '__main__':
     unittest.main()
