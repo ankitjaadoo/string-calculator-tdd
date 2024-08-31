22e5341d67148554c9aaff7fc429b325f7b2f695
Ankit Kumar <talk2ankit007@gmail.com>
2024-08-31 12:46:22 +0530

    added test case to handle negative numbers

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -19,6 +19,11 @@ class TestStringCalculator(unittest.TestCase):
     def test_custom_delimiter(self):
         self.assertEqual(add("//;\n1;2"), 3)
         self.assertEqual(add("//|\n1|2|3"), 6)
+    
+    def test_negative_numbers(self):
+        with self.assertRaises(ValueError) as context:
+            add("1,-2,3,-4")
+        self.assertEqual(str(context.exception), "negative numbers not allowed: -2,-4")
 
 if __name__ == '__main__':
     unittest.main()
