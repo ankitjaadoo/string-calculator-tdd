230602af6455fe6699de332708b93927dcf2e9d5
Ankit Kumar <talk2ankit007@gmail.com>
2024-09-05 17:39:25 +0530

    added unit tests to accommodate multiply feature based on input character

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -4,14 +4,20 @@ from string_calculator import add
 
 class TestStringCalculator(unittest.TestCase):
     
-    def test_empty_string(self):
-        self.assertEqual(add(""), 0)
+    def test_multiply_multiple_numbers(self):
+        self.assertEqual(add("2,2", '*'),4)
+
+    def test_add_empty_string(self):
+        self.assertEqual(add("", '+'), 0)
     
-    def test_single_number(self):
-        self.assertEqual(add("1"), 1)
+    def test_add_single_number(self):
+        self.assertEqual(add("1", '+'), 1)
     
-    def test_two_numbers(self):
-        self.assertEqual(add("1,2"), 3)
+    def test_add_two_numbers(self):
+        self.assertEqual(add("1,2", '+'), 3)
+    
+    def test_add_two_numbers_random_parameter(self):
+        self.assertEqual(add("1,2", '$'), 3)
     
     def test_newlines_between_numbers(self):
         self.assertEqual(add("1\n2,3"), 6)
@@ -24,6 +30,11 @@ class TestStringCalculator(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             add("1,-2,3,-4")
         self.assertEqual(str(context.exception), "negative numbers not allowed: -2,-4")
+    
+    def test_multiply_single_number(self):
+        with self.assertRaises(ValueError) as context:
+            add("1", '*')
+        self.assertEqual(str(context.exception), "need at least two numbers to multiply")
 
 if __name__ == '__main__':
     unittest.main()
