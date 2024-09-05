d111bb614219431b93efc8bdec3916f042e5f13d
Ankit Kumar <talk2ankit007@gmail.com>
2024-09-05 17:49:05 +0530

    removed extra test cases

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -20,21 +20,16 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(add("1,2", '$'), 3)
     
     def test_newlines_between_numbers(self):
-        self.assertEqual(add("1\n2,3"), 6)
+        self.assertEqual(add("1\n2,3", '+'), 6)
     
     def test_custom_delimiter(self):
-        self.assertEqual(add("//;\n1;2"), 3)
-        self.assertEqual(add("//|\n1|2|3"), 6)
+        self.assertEqual(add("//;\n1;2", '+'), 3)
+        self.assertEqual(add("//|\n1|2|3", '+'), 6)
     
     def test_negative_numbers(self):
         with self.assertRaises(ValueError) as context:
             add("1,-2,3,-4")
         self.assertEqual(str(context.exception), "negative numbers not allowed: -2,-4")
-    
-    def test_multiply_single_number(self):
-        with self.assertRaises(ValueError) as context:
-            add("1", '*')
-        self.assertEqual(str(context.exception), "need at least two numbers to multiply")
 
 if __name__ == '__main__':
     unittest.main()
